b9244927016606d0ed39f114fd98a68d24f26e1d
rusli abdul gani <abdul.gani@fullertonhealth.com>
2025-05-19 12:10:44 +0700

    tambah beberapa file

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Geist, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const geist = Geist({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
